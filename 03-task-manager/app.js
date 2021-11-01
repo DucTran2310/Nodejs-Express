d5c4708cf82f24ecceccbb0eab778c3b52f7917c
DucTran2310 <18520021@gm.uit.edu.vn>
2021-11-01 22:19:02 +0700

    Project task manager

---
+++ 03-task-manager/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')          - get all the tasks
+// app.post('/api/v1/tasks')         - create a new task
+// app.get('/api/v1/tasks/:id')      - get single task
+// app.patch('/api/v1/tasks/:id')    - update task
+// app.delete('/api/v1/tasks/:id')   - delete task
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
